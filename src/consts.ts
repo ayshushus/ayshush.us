98d00d24765eb3e5956eab26c797691f64ee0c1f
Ayush Ranjan <ayshushus@gmail.com>
2026-05-15 22:39:20 -0400

    removed README, updated content to reflect self

--- src/consts.ts
+++ src/consts.ts
@@ -1,16 +1,16 @@
 import type { Metadata, Site, Socials } from "@types";
 
 export const SITE: Site = {
-  TITLE: "Astro Micro",
-  DESCRIPTION: "Astro Micro is an accessible and lightweight blog.",
-  EMAIL: "trevortylerlee@gmail.com",
+  TITLE: "Ayush Ranjan",
+  DESCRIPTION: "Ayush Ranjan's personal website.",
+  EMAIL: "ayshushus@gmail.com",
   NUM_POSTS_ON_HOMEPAGE: 5,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
 };
 
 export const HOME: Metadata = {
-  TITLE: "Home",
-  DESCRIPTION: "Astro Micro is an accessible theme for Astro.",
+  TITLE: "Ayush Ranjan",
+  DESCRIPTION: "Ayush Ranjan's personal website.",
 };
 
 export const BLOG: Metadata = {
@@ -26,15 +26,15 @@ export const PROJECTS: Metadata = {
 
 export const SOCIALS: Socials = [
   {
-    NAME: "X (formerly Twitter)",
-    HREF: "https://twitter.com/boogerbuttcheek",
+    NAME: "GitHub",
+    HREF: "https://github.com/ayshushus",
   },
   {
-    NAME: "GitHub",
-    HREF: "https://github.com/trevortylerlee",
+    NAME: "LinkedIn",
+    HREF: "https://linkedin.com/in/ayshushus",
   },
   {
     NAME: "Website",
-    HREF: "https://trevortylerlee.com",
+    HREF: "https://ayshush.us",
   },
 ];
